4f6eb6b4b617b7513f0edeae4a70514e34cfdff9
akash singh <akashcool271995>
2023-06-15 17:02:15 +0530

    fiiirst commit

---
+++ src/App.jsx
@@ -0,0 +1,336 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+   
+      <div className='main'>
+    
+        <img className='logo' src='/Images/METRICX-GIF.gif' alt='pic' />
+
+        <h2 className="text">Transforming Data Into Actionable Intelligence</h2>
+      </div>
+
+      <div>
+        <div className="block" >
+          <img className='pic' src='/Images/rn2.jpg' alt='photo' />
+          <div className='content'>
+            <h1 className="second">Leverage the Power of Data</h1>
+
+
+
+            <p>In today&#8217;s data-driven world, businesses can gain a competitive edge by harnessing data&#8217;s power.
+              Data holds valuable insights that inform decisions, optimize operations, and enhance
+              customer experiences. It enables personalization, targeted marketing, and agility.
+              Embracing data empowers businesses to make smart decisions, optimize operations, and unlock growth potential.
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <div className='block2'>
+              <img className='pic2' src='/Images/data-12-scaled.jpg' alt='photo' />
+              <div className='content2'>
+                <h2>
+                  Why Data?
+                </h2>
+
+
+                <p>
+                  Data analytics is crucial for businesses as it enables informed decision-making, provides a competitive edge,
+                  improves efficiency, personalizes customer experiences, manages risks, and fosters continuous improvement.
+                  By leveraging data, businesses gain valuable insights into their operations, customers, and market trends,
+                  empowering them to make strategic decisions, optimize processes, and drive exponential growth in today’s
+                  data-driven world.</p>
+
+                <h2>Why Analytics?</h2>
+                <p>
+                  Analytics plays a crucial role in extracting insights from data for businesses. Descriptive analytics summarizes
+                  historical data, providing an understanding of past trends. Diagnostic analytics uncovers the reasons behind
+                  specific outcomes. Predictive analytics uses historical data and statistical models to forecast future trends.
+                  Prescriptive analytics goes a step further, offering recommendations on the best actions to take.
+                  Text analytics extracts insights from unstructured textual data. Spatial analytics focuses on analyzing
+                  location-based data. By leveraging these different types of analytics, businesses can gain a comprehensive
+                  understanding of their data, make informed decisions, and drive growth.
+                </p>
+
+                <div class="button">
+                  <div class="btn_1">
+                    <a class="btn1"   >Descriptive</a>
+                  </div>
+
+
+
+                  <div class="btn_2">
+                    <a class="btn2">Predictive
+                    </a></div>
+
+
+
+                  <div class="btn_3">
+                    <a class="btn3" >Prescriptive
+                    </a></div>
+
+
+
+                  <div class="btn_4">
+                    <a class="btn4">Diagnostic</a>
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+
+          <div className='block3'>
+            <div className='content 3'>
+              <div>
+                <h2 class="text3">Future with Data Analytics</h2>
+
+
+                <p>The future holds immense possibilities for all industries, and at the heart of this transformation lies the power of data analytics. As technology continues to advance at an unprecedented pace, organizations across sectors are realizing the untapped potential of their data. From healthcare to finance, manufacturing to retail, data analytics is poised to revolutionize the way businesses operate, make decisions, and drive growth.</p>
+
+                <p>In the healthcare industry, data analytics promises to enhance patient outcomes through personalized treatment plans, early detection of diseases, and optimized healthcare delivery. Financial institutions can leverage data analytics to detect fraud, assess risk, and provide tailored financial solutions to customers. In manufacturing, data analytics enables predictive maintenance, supply chain optimization, and improved operational efficiency.</p>
+
+                <p> Retailers can leverage data analytics to understand customer behavior, personalize shopping experiences, and optimize inventory management. Transportation and logistics companies can use data analytics to streamline routes, reduce costs, and improve delivery efficiency. The applications are endless, and the impact is profound. </p>
+
+                <p>With the advancement of artificial intelligence and machine learning, data analytics will continue to evolve, providing deeper insights, faster processing, and more accurate predictions. Real-time data analytics will enable businesses to make agile decisions, adapt to market changes, and stay ahead of the competition.</p>
+
+                <p> However, the future of data analytics isn’t limited to large corporations. Small and medium-sized enterprises will also benefit from accessible and affordable analytics tools, democratizing data-driven decision-making across industries. As we move forward, embracing data analytics will be paramount for businesses of all sizes.</p>
+
+                <p>Those who harness the power of data will gain a competitive edge, unlock new opportunities, and drive innovation. The future with data analytics is a future where industries thrive, customers receive personalized experiences, and organizations achieve unprecedented levels of success. Are you ready to embrace this future and unlock the true potential of your business? Join us on this transformative journey as we navigate the exciting possibilities of data analytics together.</p>
+
+
+                <div className='button3'>
+                  <div className='btn_3'>
+                    <a className='btn5'>GET STARTED </a>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <div className="block4">
+            <div className="content4">
+              <h2 className="text4">Empowering Your Business with Data </h2>
+            </div>
+
+            {/* <div className="block5"> */}
+            <div className="content5">
+              <div className="pic5">
+                <img className='pics5' src='/Images/DATA-5-scaled.jpg' alt='pic' />
+
+
+                <h2 className="text5">Data Visualization and Reporting
+                </h2>
+
+
+
+                <p>Transforming complex data into visual representations is our expertise. Our team utilizes
+                  cutting-edge tools and techniques to create interactive dashboards and reports that
+                  provide a comprehensive
+                  view of your data. With intuitive visualizations, you can identify trends, patterns, and
+                  correlations, enabling data-driven decision-making at every level of your organization.
+                </p>
+
+
+
+                <div className="wp-block-buttons is-layout-flex">
+                  <div className="wp-block-button is-style-button"><a
+                    className="wp-block-button__link wp-element-button">Read More</a>
+                  </div>
+                </div>
+                {/* </div> */}
+              </div>
+
+              {/* <div className="block6"> */}
+              {/* <div className="content5"> */}
+              <div className='pic5'>
+                <img className='pics6' src='/Images/DATA3-scaled.jpg' alt='pic' />
+
+
+                <h2 class="wp-block-heading has-content-heading-font-size">Predictive Analytics &amp;
+                  Forecasting</h2>
+
+
+
+                <p>Stay ahead of the competition with our predictive analytics and forecasting services. By
+                  leveraging advanced statistical models and machine learning algorithms, we help you
+                  predict future trends,
+                  customer behavior, demand patterns, and more. Our accurate forecasts empower you to make
+                  proactive decisions, optimize resources, and seize opportunities.</p>
+
+
+
+                <div className="wp-block-buttons is-layout-flex">
+                  <div className="wp-block-button is-style-button"><a
+                    className="wp-block-button__link wp-element-button">Read More</a></div>
+                </div>
+              </div>
+
+
+              <div className="pic5">
+                <img className='pics7' src='/Images/data-9-scaled.jpg' alt='pic' />
+
+
+                <h2 className="text5">Data Mining and Text Analytics
+                </h2>
+
+
+
+                <p>Uncover hidden insights from your data repositories using our data mining and text
+                  analytics services. Our experts employ sophisticated techniques to extract valuable
+                  information from structured
+                  and unstructured data sources. By analyzing text data, sentiment, and topics, we provide
+                  actionable insights to enhance customer experience, optimize processes, and drive
+                  growth.
+                </p>
+
+
+
+                <div className="wp-block-buttons is-layout-flex">
+                  <div className="wp-block-button is-style-button"><a
+                    className="wp-block-button__link wp-element-button">Read More</a>
+                  </div>
+                </div>
+                {/* </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='block_7'>
+                  <div className='block7'>
+                    <div className="text6">
+                     
+                            <h2 >Our Vision</h2>
+                      
+                            <p>Our vision is to be a leading player in the data analytics and management consulting
+                                field, empowering organizations to unlock the true potential of their data. We strive to
+                                leverage the power of
+                                AI and ML technologies to provide innovative solutions that drive strategic
+                                decision-making, optimize business processes, and enable our clients to stay ahead in
+                                the rapidly evolving digital
+                                landscape. With our deep expertise, cutting-edge tools, and commitment to excellence, we
+                                aim to be the go-to partner for organizations seeking to harness the full value of their
+                                data.</p>
+
+
+
+                            <p>Metricx envisions a future where data analytics and management consulting are seamlessly
+                                integrated, empowering organizations to make data-driven decisions with confidence. As a
+                                leader in the field,
+                                MetricX aims to revolutionize the way businesses harness the power of data to drive
+                                growth, efficiency, and innovation.</p>
+
+
+
+                            <h2 className="wp-block-heading has-text-align-left">Our Mission</h2>
+
+
+
+                            <p>Our mission at MetricX is to empower organizations to make data-driven decisions and
+                                achieve sustainable growth through our comprehensive data analytics and management
+                                consulting services. We are
+                                dedicated to helping our clients overcome their most complex challenges by providing
+                                actionable insights, developing tailored strategies, and implementing advanced
+                                technologies. Our team of experts
+                                combines domain knowledge with state-of-the-art AI and ML capabilities to deliver
+                                innovative solutions that maximize efficiency, optimize operations, and drive
+                                bottom-line results.</p>
+
+
+
+                            <p>As we continue to grow at a rapid pace, we are also committed to developing in-house
+                                products based on AI and ML. These products are designed to augment our service
+                                offerings, streamline data analysis
+                                processes, and enhance decision-making capabilities for our clients. By harnessing the
+                                power of AI and ML, we aim to provide scalable and efficient solutions that deliver
+                                accurate predictions,
+                                uncover hidden patterns, and enable proactive decision-making in real-time. Our in-house
+                                products are developed with a focus on usability, reliability, and security, ensuring
+                                that our clients
+                                can trust the insights and recommendations derived from these advanced technologies.</p>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className='container'>
+                      <div className='form'>
+                        <div className='form_left'>
+                      <h2> Contact Us</h2>
+                      <label>Name*</label>
+                      <input type='text'/>
+                      <label>Phone*</label>
+                      <input type='text'/>
+                      <label>Email*</label>
+                      <input type='text'/>
+                      <label>Message*</label>
+                      <input type='text'/>
+                      <button className='sub_btn'>Submit</button>
+                      </div>
+                      
+                      <div className='form_right'>
+                        <h2>Request Quote</h2>
+                        <label>Organization Name*</label>
+                      <input type='text'/>
+                      <label>Name*</label>
+                      <input type='text'/>
+                      <label>Email*</label>
+                      <input type='text'/>
+                      <label>Describe your project*</label>
+                      <input type='text'/>
+                      <button className='sub_btn1'>Submit</button>
+                      </div>
+                      </div>
+                    </div>
+                    
+          
+        
+                    <div className="block8">
+                        <div className='block_8'>
+                            <div className="text8">
+                                <h2 className="heading8"
+                                    >About
+                                </h2>
+
+
+
+                                <p>Metricx is revolutionizing the way businesses grow by harnessing the power of data
+                                    analytics. We empower organizations to make informed decisions, drive innovation,
+                                    and achieve exponential growth.
+                                    With our cutting-edge tools and expertise, we transform raw data into actionable
+                                    insights, unlocking hidden opportunities and driving business success. Trust Metricx
+                                    to be your strategic
+                                    partner in navigating the dynamic landscape of data-driven growth, propelling your
+                                    business to new heights.</p>
+                            </div>
+                        </div>
+                        <div className="block9">
+                            <div className="block_9">
+                                <h2 className="heading9"
+                                    >Contact
+                                </h2>
+                                <div> <p> 10:00 To 18:00</p></div>
+                                <div>  <p>+91 8459690667</p></div>
+                                <div><p>metricx@gmail.com
+                                    </p></div>
+                                    <div><p>Pune, Maharashtra, India.</p></div>
+                                </div>
+                                </div>
+                                </div>
+                                <div><p class="footer">Copyright © 2023 Metricx</p></div>
+
+
+    </>
+  )
+}
+
+export default App
